079f5ad3918cd9926904009b49e2c04437713604
Max stevenson <maxstevenson@Maxs-Mac-mini.local>
2025-06-18 21:46:40 -0700

    feat: styled homepage + script list with titles and descriptions

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
-from flask import Flask, send_from_directory
-import os 
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
+# List of scripts
+scripts = [
+    {
+        "filename": "WhyDidMyPuterCrashScript.sh",
+        "title": "Crash Recovery Script",
+        "description": "Diagnose why your puter had a mental breakdown 💀",
+    },
+    {
+        "filename": "SlayStartupScript.sh",
+        "title": "Slay Startup Script",
+        "description": "Makes your Mac start up with ✨vibes✨. Also does cleanup.",
+    },
+    # Add more here!
+]
+
 @app.route('/')
 def home():
-    return '<h1>Welcome to Max’s Script Server 😎</h1><p><a href="/grab-script">Grab the script here</a></p>'
+    return render_template('index.html', scripts=scripts)
 
-@app.route('/grab-script')
-def grab_script():
-    return send_from_directory('static', 'WhyDidMyPuterCrashScript.sh', as_attachment=True)
+@app.route('/grab/<filename>')
+def grab_script(filename):
+    return send_from_directory('static', filename, as_attachment=True)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
