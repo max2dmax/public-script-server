bbcc8767438e758b37bebe3270f53c9f4dd09b00
Max Stevenson <you@example.com>
2025-07-07 13:04:44 -0700

    Batman Mode Update

--- static/Batman_Mode.sh
+++ static/Batman_Mode.sh
@@ -2,6 +2,9 @@
 
 
 osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to true'
-
+osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/maxstevenson/Dev/public-script-server/static/images/batman_wallpaper.jpg"'
 
 say -v Daniel "Batman Mode enabled! Welcome to the bat cave, Bruce Wayne."
+osascript -e 'tell application "Terminal" to do script "echo 🦇 Welcome to Batman Mode 🦇; echo \"I am vengeance. I am the night. I am Batman.\"; echo \"\"; figlet Batman"'
+
+afplay "/Users/maxstevenson/Dev/public-script-server/static/audio/batman_theme.mp3"
